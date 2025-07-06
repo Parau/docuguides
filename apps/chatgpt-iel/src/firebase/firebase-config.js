e8278e4a946390fa36183cc91a8cc5566aa22c3b
APRENDER.digital <info@aprender.digital>
2025-07-06 19:57:10 -0300

    erro build

--- apps/chatgpt-iel/src/firebase/firebase-config.js
+++ apps/chatgpt-iel/src/firebase/firebase-config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: process.env.DOCUSaurus_FIREBASE_API_KEY,
-  authDomain: process.env.DOCUSaurus_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.DOCUSaurus_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.DOCUSaurus_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.DOCUSaurus_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.DOCUSaurus_FIREBASE_APP_ID,
+  apiKey: process.env.DOCUSAURUS_FIREBASE_API_KEY,
+  authDomain: process.env.DOCUSAURUS_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.DOCUSAURUS_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.DOCUSAURUS_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.DOCUSAURUS_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.DOCUSAURUS_FIREBASE_APP_ID,
 };
